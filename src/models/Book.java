83b07644d3b0989cb66e0b200b8bb3dd5c3caaaf
Shreya Mohanta <mohanta.shreya2405@gmail.com>
2026-09-23 03:51:24 +0530

    Add files via upload

--- src/models/Book.java
+++ src/models/Book.java
@@ -1,1 +1,40 @@
+package models;
 
+public class Book {
+
+    private int bookId;
+    private String title;
+    private String author;
+    private String genre;
+
+    public Book(int bookId, String title, String author, String genre) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Book ID: " + bookId +
+               "\nTitle: " + title +
+               "\nAuthor: " + author +
+               "\nGenre: " + genre;
+    }
+}
