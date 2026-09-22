71cb7ee7715148e2d91f24cd6dc082deb3d0c4f1
Shreya Mohanta <mohanta.shreya2405@gmail.com>
2026-09-23 03:43:25 +0530

    Add files via upload

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,1 +1,14 @@
+package database;
 
+import java.util.ArrayList;
+import models.User;
+import models.Book;
+import models.Review;
+
+public class Database {
+
+    public static ArrayList<User> users = new ArrayList<>();
+    public static ArrayList<Book> books = new ArrayList<>();
+    public static ArrayList<Review> reviews = new ArrayList<>();
+
+}
