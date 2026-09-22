128ab55d3a6a6f79281d66fb0807d8f3630da7bb
Shreya Mohanta <mohanta.shreya2405@gmail.com>
2026-09-23 03:52:55 +0530

    Add files via upload

--- src/models/User.java
+++ src/models/User.java
@@ -1,1 +1,51 @@
+package models;
 
+public class User {
+
+    private int userId;
+    private String name;
+    private String email;
+    private String password;
+
+    public User(int userId, String name, String email, String password) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User ID: " + userId +
+               "\nName: " + name +
+               "\nEmail: " + email;
+    }
+}
