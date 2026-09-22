be24e20b201634dbb9f733419a692169f54e35b8
Shreya Mohanta <mohanta.shreya2405@gmail.com>
2026-09-23 03:52:02 +0530

    Add files via upload

--- src/models/Review.java
+++ src/models/Review.java
@@ -1,1 +1,58 @@
+package models;
 
+public class Review {
+
+    private int reviewId;
+    private int userId;
+    private int bookId;
+    private int rating;
+    private String reviewText;
+
+    public Review(int reviewId, int userId, int bookId,
+                  int rating, String reviewText) {
+
+        this.reviewId = reviewId;
+        this.userId = userId;
+        this.bookId = bookId;
+        this.rating = rating;
+        this.reviewText = reviewText;
+    }
+
+    public int getReviewId() {
+        return reviewId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Review ID: " + reviewId +
+               "\nUser ID: " + userId +
+               "\nBook ID: " + bookId +
+               "\nRating: " + rating +
+               "\nReview: " + reviewText;
+    }
+}
