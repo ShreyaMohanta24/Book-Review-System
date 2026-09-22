6991dbc92ddca5a275c23bd3032ae06556132867
Shreya Mohanta <mohanta.shreya2405@gmail.com>
2026-09-23 03:49:59 +0530

    Add files via upload

--- src/manager/UserManager.java
+++ src/manager/UserManager.java
@@ -1,1 +1,57 @@
+package manager;
 
+import java.util.Scanner;
+
+import database.Database;
+import models.User;
+
+public class UserManager {
+
+    Scanner sc = new Scanner(System.in);
+
+    public void registerUser() {
+
+        System.out.print("Enter User ID: ");
+        int userId = sc.nextInt();
+        sc.nextLine();
+
+        // Check duplicate User ID
+        for (User u : Database.users) {
+            if (u.getUserId() == userId) {
+                System.out.println("User ID already exists!");
+                return;
+            }
+        }
+
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Email: ");
+        String email = sc.nextLine();
+
+        System.out.print("Enter Password: ");
+        String password = sc.nextLine();
+
+        User user = new User(userId, name, email, password);
+
+        Database.users.add(user);
+
+        System.out.println("User Registered Successfully!");
+    }
+
+    public void displayUsers() {
+
+        if (Database.users.isEmpty()) {
+            System.out.println("No users found.");
+            return;
+        }
+
+        System.out.println("\n===== USERS =====");
+
+        for (User user : Database.users) {
+            System.out.println(user);
+            System.out.println("---------------------");
+        }
+    }
+
+}
